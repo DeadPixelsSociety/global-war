4bf2353c7fb61b96af596a000920029cead76aa5
Arthur HUGEAT <hugeat.arthur@gmail.com>
2019-01-20 13:56:41 +0100

    [core] Wrap the game loop inside GameState to handle the different game loops.

---
+++ code/bits/client/GameState.cc
@@ -0,0 +1,169 @@
+#include "GameState.h"
+
+#include <gf/Color.h>
+#include <gf/Vector.h>
+
+namespace gw {
+  static constexpr gf::Vector2u ScreenSize(1024, 576);
+  static constexpr gf::Vector2f ViewSize(1000.0f, 1000.0f); // dummy values
+  static constexpr gf::Vector2f ViewCenter(0.0f, 0.0f); // dummy values
+
+  GameState::GameState(char* hostname, char *port, gf::ResourceManager &resources)
+  : m_window("Global War", ScreenSize)
+  , m_renderer(m_window)
+  , m_mainView(ViewCenter, ViewSize)
+  , m_adaptor(m_renderer, m_mainView)
+  , m_closeWindowAction("Close window")
+  , m_fullscreenAction("Fullscreen")
+  , m_leftAction("Left")
+  , m_rightAction("Right")
+  , m_upAction("Up")
+  , m_downAction("Down")
+  , m_pingAction("Ping")
+  , m_map(resources.getAbsolutePath("map.txt"))
+  , m_army(resources)
+  , m_armySelection(m_map, m_army, m_renderer, m_mainView) {
+    // Init screen
+    m_window.setVerticalSyncEnabled(true);
+    m_window.setFramerateLimit(60);
+
+    // Init network
+    m_socket.connectTo(hostname, port);
+
+    // Receive ID
+    auto packet = newPlayer();
+    m_playerID = packet.newPlayer.playerID;
+    gf::Log::info("Player ID: %lx\n", m_playerID);
+    quickMacth();
+
+    // Init views
+    m_gameViews.addView(m_mainView);
+    m_gameViews.addView(m_hudView);
+    m_gameViews.setInitialScreenSize(ScreenSize);
+
+    // Init actions
+    // for game
+    m_closeWindowAction.addCloseControl();
+    m_closeWindowAction.addKeycodeKeyControl(gf::Keycode::Escape);
+    m_gameActions.addAction(m_closeWindowAction);
+
+    m_fullscreenAction.addKeycodeKeyControl(gf::Keycode::F);
+    m_gameActions.addAction(m_fullscreenAction);
+
+    m_leftAction.addScancodeKeyControl(gf::Scancode::A);
+    m_leftAction.addScancodeKeyControl(gf::Scancode::Left);
+    m_leftAction.setContinuous();
+    m_gameActions.addAction(m_leftAction);
+
+    m_rightAction.addScancodeKeyControl(gf::Scancode::D);
+    m_rightAction.addScancodeKeyControl(gf::Scancode::Right);
+    m_rightAction.setContinuous();
+    m_gameActions.addAction(m_rightAction);
+
+    m_upAction.addScancodeKeyControl(gf::Scancode::W);
+    m_upAction.addScancodeKeyControl(gf::Scancode::Up);
+    m_upAction.setContinuous();
+    m_gameActions.addAction(m_upAction);
+
+    m_downAction.addScancodeKeyControl(gf::Scancode::S);
+    m_downAction.addScancodeKeyControl(gf::Scancode::Down);
+    m_downAction.setContinuous();
+    m_gameActions.addAction(m_downAction);
+
+    m_pingAction.addScancodeKeyControl(gf::Scancode::P);
+    m_gameActions.addAction(m_pingAction);
+
+    // Init entities
+    m_gameEntities.addEntity(m_map);
+    m_gameEntities.addEntity(m_army);
+  }
+
+  void GameState::loop() {
+    m_renderer.clear(gf::Color::White);
+
+    gf::Clock clock;
+
+    while (m_window.isOpen()) {
+      // 1. input
+
+      gf::Event event;
+
+      while (m_window.pollEvent(event)) {
+        m_gameActions.processEvent(event);
+        m_gameViews.processEvent(event);
+        m_adaptor.processEvent(event);
+        m_armySelection.processEvent(event);
+      }
+
+      if (m_closeWindowAction.isActive()) {
+        m_window.close();
+      }
+
+      if (m_fullscreenAction.isActive()) {
+        m_window.toggleFullscreen();
+      }
+
+      if (m_rightAction.isActive()) {
+        // do something
+      } else if (m_leftAction.isActive()) {
+        // do something
+      } else if (m_upAction.isActive()) {
+        // do something
+      } else if (m_downAction.isActive()) {
+        // do something
+      } else {
+        // do something
+      }
+
+      if (m_pingAction.isActive()) {
+        ping();
+      }
+
+      // 2. update
+
+      gf::Time time = clock.restart();
+      m_gameEntities.update(time);
+      m_hudEntities.update(time);
+
+
+      // 3. draw
+
+      m_renderer.clear();
+
+      m_renderer.setView(m_mainView);
+      m_gameEntities.render(m_renderer);
+
+      m_renderer.setView(m_hudView);
+      m_hudEntities.render(m_renderer);
+
+      m_renderer.display();
+
+      m_gameActions.reset();
+    }
+  }
+
+  void GameState::ping() {
+    gw::Packet packet;
+    packet.type = PacketType::Ping;
+    packet.ping.sequence = 42;
+
+    m_socket.send(packet);
+  }
+
+  void GameState::quickMacth() {
+    Packet packet;
+    packet.type = PacketType::QuickMatch;
+    packet.quickMatch.playerID = m_playerID;
+
+    m_socket.send(packet);
+  }
+
+  Packet GameState::newPlayer() {
+    Packet packet;
+    m_socket.receive(packet);
+
+    assert(packet.type == PacketType::NewPlayer);
+
+    return packet;
+  }
+}
---
+++ code/bits/client/GameState.h
@@ -0,0 +1,71 @@
+#ifndef BITS_CLIENT_GAME_STATE_H
+#define BITS_CLIENT_GAME_STATE_H
+
+#include <gf/Action.h>
+#include <gf/Entity.h>
+#include <gf/EntityContainer.h>
+#include <gf/RenderWindow.h>
+#include <gf/ResourceManager.h>
+#include <gf/ViewContainer.h>
+#include <gf/Views.h>
+#include <gf/Window.h>
+
+#include "../common/Packet.h"
+#include "../common/Sockets.h"
+
+#include "ArmySelection.h"
+#include "ClientArmy.h"
+#include "ClientMap.h"
+
+namespace gw {
+  class GameState: public gf::Entity {
+  public:
+    GameState(char* hostname, char *port, gf::ResourceManager &resources);
+
+    void loop();
+
+  private:
+    void ping();
+    void quickMacth();
+    Packet newPlayer();
+
+  private:
+    // Screen
+    gf::Window m_window;
+    gf::RenderWindow m_renderer;
+
+    // Network
+    SocketTcp m_socket;
+    gf::Id m_playerID;
+
+    // Views
+    // for game
+    gf::ViewContainer m_gameViews;
+    gf::ExtendView m_mainView;
+    gf::ScreenView m_hudView;
+    gf::ZoomingViewAdaptor m_adaptor;
+
+    // Actions
+    // for game
+    gf::ActionContainer m_gameActions;
+    gf::Action m_closeWindowAction;
+    gf::Action m_fullscreenAction;
+    gf::Action m_leftAction;
+    gf::Action m_rightAction;
+    gf::Action m_upAction;
+    gf::Action m_downAction;
+    gf::Action m_pingAction;
+
+    // Entities
+    // for game
+    gf::EntityContainer m_gameEntities;
+    ClientMap m_map;
+    ClientArmy m_army;
+    gf::EntityContainer m_hudEntities;
+
+    // State machines
+    ArmySelection m_armySelection;
+  };
+}
+
+#endif // BITS_CLIENT_GAME_STATE_H
